3654b3389aa7106ae7e4505c06232ea8d2e9fe4f
Li Xu <lixu@garvanITs-MacBook-Pro.local>
2017-09-04 19:13:45 +1000

    ..

--- fusion-angular/src/app/app.module.ts
+++ fusion-angular/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { HomeComponent } from './home/home.component';
+
+const rootRouting: ModuleWithProviders = RouterModule.forRoot([], { useHash: true });
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeModule,
+    rootRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
