ece6ed748fd407810c87d0f7fc28925c53209b05
Li Xu <jcuwundt@gmail.com>
2019-11-27 11:28:57 +1100

    fixed tests

--- ng-olmap/src/app/app.component.spec.ts
+++ ng-olmap/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'ng-map'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-map');
+    expect(app.title).toEqual('nearmap');
   });
 
   it('should render title in a h1 tag', () => {
